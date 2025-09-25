3fc3169a864f87208313df7d0b734605c6390025
David Crespo <david.crespo@oxidecomputer.com>
2025-09-25 12:16:29 -0500

    add integration test for tuf repo list

--- nexus/db-queries/src/db/datastore/update.rs
+++ nexus/db-queries/src/db/datastore/update.rs
@@ -227,7 +227,6 @@ impl DataStore {
             (dsl::time_created, dsl::id),
             pagparams,
         )
-        .order_by((dsl::time_created.desc(), dsl::id))
         .select(TufRepo::as_select())
         .load_async(&*conn)
         .await
